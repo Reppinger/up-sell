0550ac7ba9bd946f6b7116b87d5bf74e656e2692
Randy Eppinger <randy.eppinger@leandog.com>
2016-12-12 10:56:09 -0500

    Replaced CheckoutButton class with simple constant because component is stateless.

--- app/checkout_button.jsx
+++ app/checkout_button.jsx
@@ -1,5 +1,9 @@
-class CheckoutButton extends React.Component {
-    render() {
+// class CheckoutButton extends React.Component {
+//     render() {
+//         return <button id="checkout" style={{fontSize: '20pt'}}>Checkout</button>
+//     }
+// }
+
+const CheckoutButton = (props) => {
         return <button id="checkout" style={{fontSize: '20pt'}}>Checkout</button>
-    }
-}
+};
