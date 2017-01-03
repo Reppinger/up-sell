638da86879c4ebc115680516335cbafe3d9fc1be
Randy Eppinger <randy.eppinger@leandog.com>
2017-01-03 13:21:23 -0500

    Add some events using default methods and handle click event of button.

--- app/checkout_button.jsx
+++ app/checkout_button.jsx
@@ -1,8 +1,24 @@
-// class CheckoutButton extends React.Component {
-//     render() {
-//         return <button id="checkout" style={{fontSize: '20pt'}}>Checkout</button>
-//     }
-// }
+class CheckoutButton extends React.Component {
+    componentWillMount() {
+        console.log(ReactDOM.findDOMNode(this))
+    }
 
-const CheckoutButton = (props) =>
-    <button id={props.id} style={{fontSize: props.fontSize}}>Checkout</button>;
+
+    componentDidMount() {
+        console.dir(ReactDOM.findDOMNode(this))
+    }
+
+    handleSave(event) {
+        console.log(this, event)
+    }
+
+    render() {
+        return <button
+            onClick={this.handleSave.bind(this)}
+            id={this.props.id}
+            style={{fontSize: this.props.fontSize}}>Checkout</button>
+    }
+}
+
+// const CheckoutButton = (props) =>
+//     <button id={props.id} style={{fontSize: props.fontSize}}>Checkout</button>;
