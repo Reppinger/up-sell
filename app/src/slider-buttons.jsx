7be4f7dc42e6fab958fbb3e3874881efc40a1eb3
Randy Eppinger <randy.eppinger@leandog.com>
2017-01-25 09:08:41 -0500

    Extracted methods and simplified logic for disabling buttons when slider is at max or min.

--- app/src/slider-buttons.jsx
+++ app/src/slider-buttons.jsx
@@ -2,16 +2,21 @@ class SliderButtons extends React.Component {
     calculateNewSliderValue(delta) {
         return (this.props.sliderValue + delta);
     }
-
+    isMax() {
+        return this.props.sliderValue >= 100;
+    }
+    isMin() {
+        return this.props.sliderValue < 1;
+    }
     render() {
         let newSliderValue = 0;
         return <div>
-            <button disabled={(this.props.sliderValue <1 ? true : false)}
+            <button disabled={this.isMin()}
                     {...newSliderValue = this.calculateNewSliderValue(-1)}
                     onClick={this.props.updateSlider(newSliderValue)}>
                 Decrease to {newSliderValue}
             </button>
-            <button disabled={(this.props.sliderValue >=100 ? true : false)}
+            <button disabled={this.isMax()}
                     {...newSliderValue = this.calculateNewSliderValue(1)}
                     onClick={this.props.updateSlider(newSliderValue)}>
                 Increase to {newSliderValue}
