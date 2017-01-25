fa0fe9e5584807e7df5b1244a0dc4bc3ce986bc1
Randy Eppinger <randy.eppinger@leandog.com>
2017-01-25 10:43:22 -0500

    Refactored very similar button code into a button method.

--- app/src/slider-buttons.jsx
+++ app/src/slider-buttons.jsx
@@ -1,26 +1,23 @@
 class SliderButtons extends React.Component {
-    calculateNewSliderValue(delta) {
-        return (this.props.sliderValue + delta);
+    calculateSliderValue(delta) {
+        return (this.props.sliderValue + delta)
     }
     isMax() {
-        return this.props.sliderValue >= this.props.sliderMax;
+        return this.props.sliderValue >= this.props.sliderMax
     }
     isMin() {
-        return this.props.sliderValue < this.props.sliderMin;
+        return this.props.sliderValue < this.props.sliderMin
+    }
+    button(disabled, newSliderValue, caption) {
+        return <button  disabled={disabled}
+                        onClick={this.props.updateSlider(newSliderValue)}>
+            {caption} {newSliderValue}
+        </button>
     }
     render() {
-        let newSliderValue = 0;
         return <div>
-            <button disabled={this.isMin()}
-                    {...newSliderValue = this.calculateNewSliderValue(-1)}
-                    onClick={this.props.updateSlider(newSliderValue)}>
-                Decrease to {newSliderValue}
-            </button>
-            <button disabled={this.isMax()}
-                    {...newSliderValue = this.calculateNewSliderValue(1)}
-                    onClick={this.props.updateSlider(newSliderValue)}>
-                Increase to {newSliderValue}
-            </button>
+            { this.button(this.isMin(), this.calculateSliderValue(-1), 'Decrease to') }
+            { this.button(this.isMax(), this.calculateSliderValue(1),  'Increase to') }
         </div>
     }
 }
